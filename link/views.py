4e6a202a453a9fe29e4e655184894d716d1c9722
Marco Marignoli <contact@marcomarignoli.it>
2014-09-26 13:09:22 +0100

    Link name is now unique

--- link/views.py
+++ link/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from link.models import Link
 import urllib
 
 def link(request,url):
-    l = Link.objects.get(link_name=url)
-    response = HttpResponse(content="", status=307)
-    response["Location"] = l.link_to
-    return response
+    try:
+        l = Link.objects.get(link_name=url)
+        response = HttpResponse(content="", status=307)
+        response["Location"] = l.link_to
+        return response
+    except Link.DoesNotExist:
+        return HttpResponseRedirect('/')
